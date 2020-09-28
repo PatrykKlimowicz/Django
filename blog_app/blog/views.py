2fd28e9cd46dc15ca1e80ad6fd7fb8ea44262b2b
Patryk Klimowicz <patryk.klimowicz@gmail.com>
2020-09-28 22:46:01 +0200

    BLOGAPP-010: Suggest for users similarly tagged posts - Create QuerySet which filters posts and choose up to 4 similar - Update post detail template - user can go to another related post

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
 
 
@@ -49,7 +50,24 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
-    context = {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form}
+    # Retrieve list of posts id which are tagged similarly to current post
+    # flat=True creates list of tags instead of list of tuples of tags
+    post_tags_ids = post.tags.values_list('id', flat=True)
+
+    # Get all posts that contains tags obtained earlier, exclude current post
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+
+    # Obtain list of posts with similar tags order by same_tags and date published
+    # (more common tags first, more recent post first)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
+    context = {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form,
+        'similar_posts': similar_posts,
+    }
     return render(request, 'blog/post/detail.html', context=context)
 
 
