fcd775de38da66d58752f9e57ff6c20f2d6dd9f9
Patryk Klimowicz <patryk.klimowicz@gmail.com>
2020-10-04 18:45:51 +0200

    BLOGAPP-016: Create search functionality for blog app - create search form - create search view - create search template

--- blog_app/blog/views.py
+++ blog_app/blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 # Create your views here.
 
 
@@ -93,3 +94,17 @@ def post_share(request, post_id):
 
     context = {'form': form, 'post': post, 'sent': sent}
     return render(request, 'blog/post/share.html', context=context)
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+
+    context = {'form': form, 'query': query, 'results': results}
+    return render(request, 'blog/post/search.html', context=context)
--- blog_app/blog_app/settings.py
+++ blog_app/blog_app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
