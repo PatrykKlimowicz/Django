51350accf8158eb45fc784d0377935d8cc15bf84
Patryk Klimowicz <patryk.klimowicz@gmail.com>
2020-10-07 20:41:02 +0200

    SOCIALAPP-006: Create password reset fucntionality for user - Use 4 built-in views that are necessery to reset password - Customize built-in views with templates - Allow 'sending' e-mail to the console

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -14,7 +14,13 @@ urlpatterns = [
     # application views
     path('', views.dashboard, name='dashboard'),
 
-    # resetting password
+    # changing password
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # resetting password
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
