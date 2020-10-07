8dec12bdd1a98d2ded1137286a68e23267635ba5
Patryk Klimowicz <patryk.klimowicz@gmail.com>
2020-10-07 19:28:00 +0200

    SOCAILAPP-003: USe Django built-in authentication log-in/out functionality - Use built-in log in and log out views - Customize it with templates - Configure settings.py to achieve desired bahavior

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 # Create your views here.
 
@@ -25,3 +26,9 @@ def user_login(request):
 
     context = {'form': form}
     return render(request, 'account/login.html', context=context)
+
+
+@login_required
+def dashboard(request):
+    context = {'section': dashboard}
+    return render(request, 'account/dashboard.html', context=context)
