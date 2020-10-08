12c13125c3f9cdcdaafe96092ad5ab018453dc0f
Patryk Klimowicz <patryk.klimowicz@gmail.com>
2020-10-08 12:21:38 +0200

    SOCIALAPP-009: Add messages to notify user about status of his action - Use django messages while user is updating their profile information

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 # Create your views here.
@@ -60,7 +62,10 @@ def edit_user(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            print("qweqasgeg")
+            messages.success(request, 'Profile updated successfully!')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Error updating Your profile!')
             return redirect('dashboard')
     else:
         user_form = UserEditForm(instance=request.user)
