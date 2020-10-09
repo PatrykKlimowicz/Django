016824e9a376a5f64e5b27d67d8bce342238f293
Patryk Klimowicz <patryk.klimowicz@gmail.com>
2020-10-09 22:48:08 +0200

    SOCIALAPP-013: Create images app and make initial configuration - Create images app - Configure model and form for uploading images - Create view and template for creating and image entry - Register new table in admin panel - Configure url paths

---
+++ bookmarks/images/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from .models import Image
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+
+
+class ImageCreateForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('title', 'url', 'description')
+        widgets = {
+            'url': forms.HiddenInput,
+        }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpeg', 'jpg']
+        extension = url.rsplit('.', 1)[1].lower()
+
+        if extension in valid_extensions:
+            return url
+        else:
+            raise forms.ValidationError('The given URL does not match valid extensions.')
+
+    def save(self, force_insert=False,
+             force_update=False,
+             commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # download the image
+        response = request.urlopen(image_url)
+        image.image.save(image_name, ContentFile(response.read()), save=False)
+
+        if commit:
+            image.save()
+        return image
